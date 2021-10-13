31b172fdd70980d1a457658c2f7fce6187cfe6ef
Aaron Machill <aaron.machill@web.de>
2021-10-13 02:33:51 +0200

    removed a useless to-console-print

--- backend/models/posts-db.go
+++ backend/models/posts-db.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -19,7 +18,6 @@ func (m *DBModel) InsertPost(post Post) (*primitive.ObjectID, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	fmt.Println(post)
 	currentTime := time.Now().UTC()
 	post.CreatedAt = &currentTime
 	post.UpdatedAt = &currentTime
